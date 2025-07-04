03ddeac5f51d696e32b8472f5b946e3c46bcb82f
Harsh Dubey <dubeyharsh320@gmail.com>
2025-07-04 12:01:01 +0530

    inital project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<Product/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
